f209c4c0d7f8e460ea13ffbec5b5b72af1bc3a1b
beingbane <beingbane96@gmail.com>
2020-12-29 16:36:45 +0530

    testNewLine

--- String-Calculator-master/src/test/java/is/ru/stringcalculator/StringCalculatorTest.java
+++ String-Calculator-master/src/test/java/is/ru/stringcalculator/StringCalculatorTest.java
@@ -28,4 +28,9 @@ public class StringCalculatorTest {
     public void testThreeNumbers(){
     	assertEquals(6, StringCalculator.add("1,2,3"));
     }
+	
+	@Test
+    public void testNewLine(){
+    	assertEquals(6, StringCalculator.add("1\n2,3"));
+    }
 }
